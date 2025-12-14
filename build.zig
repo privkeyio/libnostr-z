3f9b4c2eb03bb570aa52aae1b0155e1280326464
Kyle 🐆 <kyle@privkey.io>
2025-12-14 09:55:16 -0500

    Rename nostr.zig to root.zig for Zig package convention (#8)

--- build.zig
+++ build.zig
@@ -10,7 +10,7 @@ pub fn build(b: *std.Build) void {
     });
 
     const nostr_mod = b.addModule("nostr", .{
-        .root_source_file = b.path("src/nostr.zig"),
+        .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -20,7 +20,7 @@ pub fn build(b: *std.Build) void {
 
     const tests = b.addTest(.{
         .root_module = b.createModule(.{
-            .root_source_file = b.path("src/nostr.zig"),
+            .root_source_file = b.path("src/root.zig"),
             .target = target,
             .optimize = optimize,
         }),
